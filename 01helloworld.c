f76d84092ff3609703cc15e2f78be1531355c176
cyberwhiteelephant <146865165+cyberwhiteelephant@users.noreply.github.com>
2024-01-05 19:27:51 +0530

    Update 01helloworld.c

--- 01helloworld.c
+++ 01helloworld.c
@@ -4,13 +4,6 @@ int main(){
     printf("Hello World");
 }
 
-
-Output:-
-PS D:\College Program\C Lab Codes> gcc 01helloworld.c
-PS D:\College Program\C Lab Codes> .\a.exe
-Hello World
-
-
 //Write a C program to print Hello World using new line character.
 #include <stdio.h>
 
@@ -19,9 +12,3 @@ int main()
     printf("Hello World\n");
     return 0;
 }
-
-
-Output:-
-PS D:\College Program\C Lab Codes> gcc 01helloworld.c
-PS D:\College Program\C Lab Codes> .\a.exe
-Hello World
